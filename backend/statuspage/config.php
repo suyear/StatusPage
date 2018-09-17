748dea6836ae1edc720e5a6f980ac95a13f06814
suyear <suyear@qq.com>
2018-09-17 17:32:20 +0800

    测试

--- backend/statuspage/config.php
+++ backend/statuspage/config.php
@@ -2,13 +2,13 @@
 namespace NerdBaggy\StatusPage;
 
 //Uptime Robot API Key
-define('apiKey', 'Your-API-Key-Here');
+define('apiKey', 'u428303-f239a3b52e01ea686042d44b');
 
 //Uptime percentage for following days
 define('historyDay', '1-7-30-360');
 
 //Display names for the uptime percentage of the previous days
-define('historyDaysNames', serialize(array('Past 24 Hours', 'Past Week', 'Past Month', 'Past Year')));
+define('historyDaysNames', serialize(array('过去24小时', '过去一周', 'Past Month', 'Past Year')));
 
 //How long to hold the cache of the data, best option is how often your checks get checked in seconds
 define('cacheTime', 300);
